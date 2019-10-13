2c4e2f4307cd1ecbea4708440053ee14f9b4d1e0
Kjetil Valstadsve <valstadsve@gmail.com>
2019-10-13 13:40:09 +0200

    Wrapper up!

--- mediaserver/src/main/java/mediaserver/gui/ProgressListener.java
+++ mediaserver/src/main/java/mediaserver/gui/ProgressListener.java
@@ -13,21 +13,21 @@ final class ProgressListener implements ChannelProgressiveFutureListener {
 
     private final File file;
 
-    public ProgressListener(File file) {
+    ProgressListener(File file) {
         this.file = file;
     }
 
     @Override
     public void operationProgressed(ChannelProgressiveFuture future, long progress, long total) {
         if (total < 0) { // total unknown
-            log.info("{} Transfer progress: {} {}", future.channel(), file, progress);
+            log.info("{} {} => {}", future.channel(), file, progress);
         } else {
-            log.info("{} Transfer progress: {} {}/{}", future.channel(), file, progress, total);
+            log.info("{} {} => {}%", future.channel(), file, 100 * progress / total);
         }
     }
 
     @Override
     public void operationComplete(ChannelProgressiveFuture future) {
-        log.info("{} Transfer complete: {}", future.channel(), file);
+        log.info("{} OK {}", future.channel(), file);
     }
 }
