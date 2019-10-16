83da3627e3f1849461e32210c3a0abf0ac5289a7
Kjetil Valstadsve <valstadsve@gmail.com>
2019-10-16 11:42:21 +0200

    Cloudio working

--- mediaserver/src/main/java/mediaserver/util/S3.java
+++ mediaserver/src/main/java/mediaserver/util/S3.java
@@ -8,7 +8,6 @@ import org.slf4j.LoggerFactory;
 import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.net.URI;
-import java.net.URL;
 import java.util.Optional;
 import java.util.function.Supplier;
 
@@ -29,10 +28,9 @@ public final class S3 {
     private static final String AWS_KEY = "awsKey";
 
     private static final String AWS_SECRET = "awsSecret";
-
-    private static final Supplier<Optional<MinioClient>> S3 = MostlyOnce.get(mediaserver.util.S3::s3);
     private static final String AWS_SECRET_ENV = "AWS_SECRET";
     private static final String AWS_KEY_ENV = "AWS_KEY";
+    private static final Supplier<Optional<MinioClient>> S3 = MostlyOnce.get(mediaserver.util.S3::s3);
 
     private S3() {
 
@@ -43,17 +41,18 @@ public final class S3 {
     }
 
     private static Optional<MinioClient> s3() {
-        String awsKey = getProperty(AWS_KEY, AWS_KEY_ENV);
-        String awsSecret = getProperty(AWS_SECRET, AWS_SECRET_ENV);
+        String awsKey = getProperty(AWS_KEY, AWS_KEY_ENV, true);
+        String awsSecret = getProperty(AWS_SECRET, AWS_SECRET_ENV, true);
         if (awsKey != null && awsSecret != null) {
             log.info("Connecting to S3 with system properties/env vars");
             return Optional.of(newClient(awsKey, awsSecret));
         }
-        String cloudUri = getProperty(RELATIVE_URI, RELATIVE_URI);
+        String cloudUri = getProperty(RELATIVE_URI, RELATIVE_URI, false);
         if (cloudUri != null) {
+            URI uri = URI.create(SELF_ASSIGNED + cloudUri);
+            log.info("Looking for credentials on {}", uri);
             try {
-                URL url = URI.create(SELF_ASSIGNED + cloudUri).toURL();
-                HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+                HttpURLConnection urlConnection = (HttpURLConnection) uri.toURL().openConnection();
                 try (InputStream inputStream = urlConnection.getInputStream()) {
                     Credentials credentials = new ObjectMapper()
                         .readerFor(Credentials.class)
@@ -74,8 +73,12 @@ public final class S3 {
         return Optional.empty();
     }
 
-    private static String getProperty(String property, String envar) {
-        return System.getProperty(property, System.getenv(envar));
+    private static String getProperty(String property, String envVar, boolean mask) {
+        String value = System.getProperty(property, System.getenv(envVar));
+        log.info("Resolved {}/{} -> {}", property, envVar, mask && value != null
+            ? "[" + value.length() + " chars]"
+            : value);
+        return value;
     }
 
     private static MinioClient newClient(String awsKey, String awsSecret) {
@@ -97,6 +100,8 @@ public final class S3 {
         private String SecretAccessKey;
 
         private String Token;
+
+        private String RoleArn;
 
         private String Expiration;
 
@@ -122,6 +127,14 @@ public final class S3 {
 
         public void setToken(String token) {
             Token = token;
+        }
+
+        public String getRoleArn() {
+            return RoleArn;
+        }
+
+        public void setRoleArn(String roleArn) {
+            RoleArn = roleArn;
         }
 
         public String getExpiration() {
