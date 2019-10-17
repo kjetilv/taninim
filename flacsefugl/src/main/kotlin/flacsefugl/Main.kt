3a4f29fe02ace0659a5abbd9ac24b0f8821c6b2c
Kjetil Valstadsve <valstadsve@gmail.com>
2019-10-17 20:15:52 +0200

    On the fly 1

--- flacsefugl/src/main/kotlin/flacsefugl/Main.kt
+++ flacsefugl/src/main/kotlin/flacsefugl/Main.kt
@@ -80,13 +80,9 @@ fun main() {
 
 fun dists(): List<Dist> = listOf<Pair<Path, (Path) -> Boolean>>(
 
-        Paths.get("Masada", "Book of Angels") to { path ->
-            album(path).contains("book of angels vol. ")
-        },
+        Paths.get("Masada", "Book of Angels") to albumContains("book of angels vol. "),
 
-        Paths.get("Masada", "The Book Beri'ah") to { path ->
-            album(path).contains("book beri'ah vol. ")
-        },
+        Paths.get("Masada", "The Book Beri'ah") to albumContains("book beri'ah vol. "),
 
         Paths.get("Masada", "Masada Book 1") to { path ->
             artist(path) == "masada" && album(path).contains("book 1 vol. ")
@@ -108,29 +104,17 @@ fun dists(): List<Dist> = listOf<Pair<Path, (Path) -> Boolean>>(
             artist(path).contains("masada") && album(path).contains(" live ")
         },
 
-        Paths.get("Masada") to { path ->
-            artist(path) == "electric masada"
-        },
+        Paths.get("Masada", "Various") to artistIs("electric masada"),
 
-        Paths.get("Masada", "Masada Book 1") to { path ->
-            album(path).contains("sanhedrin")
-        },
+        Paths.get("Masada", "Masada Book 1") to albumContains("Sanhedrin"),
 
-        Paths.get("Hardcore miniatures", "Painkiller") to { path ->
-            artist(path).startsWith("painkiller")
-        },
+        Paths.get("Hardcore miniatures", "Painkiller") to artistContains("painkiller"),
 
-        Paths.get("Hardcore miniatures", "Naked City") to { path ->
-            artist(path) == "naked city"
-        },
+        Paths.get("Hardcore miniatures", "Naked City") to artistIs("Naked City"),
 
-        Paths.get("Cobra") to { path ->
-            album(path).contains("cobra")
-        },
+        Paths.get("Cobra") to albumContains("Cobra"),
 
-        Paths.get("John Zorn") to { path ->
-            artist(path).contains("john zorn")
-        },
+        Paths.get("John Zorn") to artistContains("John Zorn"),
 
         Paths.get("On Tzadik") to { path ->
             artist(path) == "makigami koichi" ||
@@ -148,6 +132,24 @@ fun dists(): List<Dist> = listOf<Pair<Path, (Path) -> Boolean>>(
         }
 ).map {
     SubdirDist(it.first, it.second)
+}
+
+private fun artistContains(name: String): (Path) -> Boolean {
+    return { path ->
+        artist(path).contains(name.toLowerCase())
+    }
+}
+
+private fun artistIs(name: String): (Path) -> Boolean {
+    return { path ->
+        artist(path) == name.toLowerCase()
+    }
+}
+
+private fun albumContains(name: String): (Path) -> Boolean {
+    return { path ->
+        album(path).contains(name.toLowerCase())
+    }
 }
 
 private fun album(path: Path) = path.parent.fileName.toString().toLowerCase()
