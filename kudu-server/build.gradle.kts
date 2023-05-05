0b5e342287755f1b529e02337ba82f355173d090
Kjetil Valstadsve <kjetilv.github@vlitejk.cotse.net>
2023-05-05 08:37:31 +0200

    Try downgrading

--- kudu-server/build.gradle.kts
+++ kudu-server/build.gradle.kts
@@ -22,8 +22,8 @@ dependencies {
 }
 
 configure<JavaPluginExtension> {
-    sourceCompatibility = JavaVersion.VERSION_19
-    targetCompatibility = JavaVersion.VERSION_19
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
 }
 
 tasks.getByName<Test>("test") {
