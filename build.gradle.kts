78382826ee39d1d24749b56d4e3961e74cb8c4d8
Kjetil Valstadsve <kjetilv.github@vlitejk.cotse.net>
2024-08-12 21:59:09 +0200

    Madness continues

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,10 +12,10 @@ subprojects {
 
     java {
         toolchain {
-            languageVersion.set(JavaLanguageVersion.of(21))
+            languageVersion.set(JavaLanguageVersion.of(22))
         }
-        sourceCompatibility = JavaVersion.VERSION_21
-        targetCompatibility = JavaVersion.VERSION_21
+        sourceCompatibility = JavaVersion.VERSION_22
+        targetCompatibility = JavaVersion.VERSION_22
         withSourcesJar()
     }
 
