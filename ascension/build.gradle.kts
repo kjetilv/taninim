29fe9c0abfbb0d056c3bd2a3d8eac2d66cbc481b
Kjetil Valstadsve <kjetilv.github@vlitejk.cotse.net>
2024-08-13 21:30:10 +0200

    Fixeered-uppered, again

--- ascension/build.gradle.kts
+++ ascension/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 }
 
 dependencies {
-    implementation("software.amazon.awscdk:aws-cdk-lib:2.124.0")
+    implementation("software.amazon.awscdk:aws-cdk-lib:2.151.0")
     implementation("software.constructs:constructs:10.3.0")
     testImplementation(platform("org.junit:junit-bom:5.10.3"))
     testImplementation("org.junit.jupiter:junit-jupiter")
